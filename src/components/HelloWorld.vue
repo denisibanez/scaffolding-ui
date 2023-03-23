7c7087ef8aaefcda505927e99eaddfd9a58233a1
Denis ibanez Rodrigues vieira <denisibanezrodriguesvieira@MacBook-Pro-de-Denis.local>
2023-03-22 22:27:31 -0300

    feature: vuetify

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {BcButton}  from '@denisibanez/design-system-ui';
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <BcButton />
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
