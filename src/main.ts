9542e500cf8cc1b274376b974526fc703ae8159e
Denis ibanez Rodrigues vieira <denisibanezrodriguesvieira@MacBook-Pro-de-Denis.local>
2023-03-22 22:51:52 -0300

    feature: quasar

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,34 @@
+// FILE: main.js
+
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import '@quasar/extras/themify/themify.css'
+import '@quasar/extras/line-awesome/line-awesome.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
 import App from './App.vue'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+const myApp = createApp(App)
 
-const vuetify = createVuetify({
-  components,
-  directives,
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
 })
 
-createApp(App).use(vuetify).mount('#app')
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
